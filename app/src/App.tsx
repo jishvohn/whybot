d6eb3d6a60a76799fea9361063cddc347cfd21b7
John Qian <johnlongqian@gmail.com>
2023-04-22 15:39:21 -0700

    tailwind, classnames, starter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,60 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Flow, FlowProvider } from "./Flow";
+import { FlowProvider } from "./Flow";
+import "./index.css";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import classNames from "classnames";
+
+function StartPage(props: { onSubmitQuery: (query: string) => void }) {
+  const [query, setQuery] = useState("");
+  return (
+    <div className="w-72 mx-auto mt-32 flex flex-col space-y-4">
+      <div>What would you like to understand?</div>
+      <div className="flex space-x-2 items-center">
+        <input
+          autoFocus
+          className="underline w-72 text-xl outline-none"
+          placeholder="Why is the meaning of life 42?"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              props.onSubmitQuery(query);
+            }
+          }}
+        />
+        <PaperAirplaneIcon
+          className={classNames("w-5 h-5", {
+            "opacity-30": !query,
+            "cursor-pointer": query,
+          })}
+          onClick={() => {
+            if (query) {
+              props.onSubmitQuery(query);
+            }
+          }}
+        />
+      </div>
+      <div>Suggest random question</div>
+    </div>
+  );
+}
 
 function App() {
+  const [seedQuery, setSeedQuery] = useState<string>();
+  const [running, setRunning] = useState(false);
   return (
     <>
-      <FlowProvider />
+      {seedQuery ? (
+        <FlowProvider />
+      ) : (
+        <StartPage
+          onSubmitQuery={(query) => {
+            setSeedQuery(query);
+          }}
+        />
+      )}
     </>
   );
 }
