150d757fe7585335fb8a7097a34adf2ad75a9e74
Vish Rajiv <vishwaesh.rajiv@gmail.com>
2023-05-04 23:07:50 -0700

    tailwind apikey modal styling + 60px stroke width for red delete edge

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,9 +12,10 @@ import {
   CheckIcon,
   ChevronUpDownIcon,
   PaperAirplaneIcon,
+  InformationCircleIcon,
 } from "@heroicons/react/24/outline";
 import classNames from "classnames";
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, Transition, Dialog } from "@headlessui/react";
 import TextareaAutosize from "react-textarea-autosize";
 import { MarkerType, Node, Edge } from "reactflow";
 
@@ -210,6 +211,132 @@ const AVAILABLE_PERSONAS = [
   { name: "Wise", value: "wise" },
 ];
 
+type APIKeyModalProps = {
+  open: boolean;
+  onClose: () => void;
+};
+export function APIKeyModal({ open, onClose }: APIKeyModalProps) {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-700 px-4 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-100">API key</p>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex outline-none rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white"
+                    onClick={onClose}
+                  >
+                    Use my own API key
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
+
+type APIInfoModalProps = {
+  open: boolean;
+  onClose: () => void;
+};
+export function APIInfoModal({ open, onClose }: APIInfoModalProps) {
+  const [isAPIKeyModalOpen, setAPIKeyModalOpen] = useState(false);
+  return (
+    <>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-700 px-4 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <div>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-100">
+                        Running these prompts is expensive, so for now we’re
+                        limiting everyone to 3 a day. However, you can bypass
+                        the limit if you have your own OpenAI API key!
+                      </p>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex outline-none rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white"
+                      onClick={() => {
+                        setAPIKeyModalOpen(true);
+                        onClose();
+                      }}
+                    >
+                      Use my own API key
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      <APIKeyModal
+        open={isAPIKeyModalOpen}
+        onClose={() => {
+          setAPIKeyModalOpen(false);
+        }}
+      />
+    </>
+  );
+}
+
 function StartPage(props: {
   onSubmitQuery: (query: string, model: string, persona: string) => void;
 }) {
@@ -222,147 +349,173 @@ function StartPage(props: {
     name: string;
     value: string;
   }>(AVAILABLE_PERSONAS[0]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className="w-[450px] mx-auto flex flex-col mt-8">
-      <div className="flex space-x-6">
-        <Listbox value={selectedModel} onChange={setSelectedModel}>
-          {({ open }) => (
-            <div className="flex items-center space-x-2">
-              <Listbox.Label className="block text-sm leading-6">
-                Model:
-              </Listbox.Label>
-              <div className="relative w-28">
-                <Listbox.Button className="relative w-full cursor-pointer rounded-md py-1.5 pl-3 pr-10 text-left shadow-sm sm:text-sm sm:leading-6 border border-white/30 hover:border-white/40">
-                  <span className="block truncate">{selectedModel.name}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ChevronUpDownIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Listbox.Button>
+      <div className="flex space-x-2">
+        <div className="flex flex-col space-y-3">
+          <div className="flex items-center space-x-4">
+            <Listbox value={selectedModel} onChange={setSelectedModel}>
+              {({ open }) => (
+                <div className="flex items-center space-x-5">
+                  <Listbox.Label className="block text-sm leading-6">
+                    Model:
+                  </Listbox.Label>
+                  <div className="relative w-28">
+                    <Listbox.Button className="relative w-full cursor-pointer rounded-md py-1.5 pl-3 pr-10 text-left shadow-sm sm:text-sm sm:leading-6 border border-white/30 hover:border-white/40">
+                      <span className="block truncate">
+                        {selectedModel.name}
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                          className="h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Listbox.Button>
 
-                <Transition
-                  show={open}
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-zinc-700 border border-white/30 py-1 shadow-lg sm:text-sm">
-                    {AVAILABLE_MODELS.map((model) => (
-                      <Listbox.Option
-                        key={model.value}
-                        className={({ active }) =>
-                          classNames(
-                            "relative cursor-pointer select-none py-2 pl-3 pr-9",
-                            { "bg-zinc-600": active }
-                          )
-                        }
-                        value={model}
-                      >
-                        {({ selected }) => (
-                          <>
-                            <span
-                              className={classNames(
-                                selected ? "font-semibold" : "font-normal",
-                                "block truncate"
-                              )}
-                            >
-                              {model.name}
-                            </span>
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-zinc-700 border border-white/30 py-1 shadow-lg sm:text-sm">
+                        {AVAILABLE_MODELS.map((model) => (
+                          <Listbox.Option
+                            key={model.value}
+                            className={({ active }) =>
+                              classNames(
+                                "relative cursor-pointer select-none py-2 pl-3 pr-9",
+                                { "bg-zinc-600": active }
+                              )
+                            }
+                            value={model}
+                          >
+                            {({ selected }) => (
+                              <>
+                                <span
+                                  className={classNames(
+                                    selected ? "font-semibold" : "font-normal",
+                                    "block truncate"
+                                  )}
+                                >
+                                  {model.name}
+                                </span>
 
-                            {selected ? (
+                                {selected ? (
+                                  <span
+                                    className={classNames(
+                                      "absolute inset-y-0 right-0 flex items-center pr-2"
+                                    )}
+                                  >
+                                    <CheckIcon
+                                      className="h-5 w-5"
+                                      aria-hidden="true"
+                                    />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </div>
+              )}
+            </Listbox>
+            <div
+              className="flex space-x-1 cursor-pointer opacity-80 hover:opacity-90"
+              onClick={() => {
+                setIsModalOpen(true);
+              }}
+            >
+              <div className="border-b border-dashed border-gray-300 text-sm text-gray-300">
+                3 prompts left
+              </div>
+              <InformationCircleIcon className="h-5 w-5 text-gray-400" />
+            </div>
+          </div>
+          <APIInfoModal
+            open={isModalOpen}
+            onClose={() => {
+              setIsModalOpen(false);
+            }}
+          />
+          <Listbox value={selectedPersona} onChange={setSelectedPersona}>
+            {({ open }) => (
+              <div className="flex items-center space-x-2">
+                <Listbox.Label className="block text-sm leading-6">
+                  Persona:
+                </Listbox.Label>
+                <div className="relative w-48">
+                  <Listbox.Button className="relative w-full cursor-pointer rounded-md py-1.5 pl-3 pr-10 text-left shadow-sm sm:text-sm sm:leading-6 border border-white/30 hover:border-white/40">
+                    <span className="block truncate">
+                      {selectedPersona.name}
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <ChevronUpDownIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-zinc-700 border border-white/30 py-1 shadow-lg sm:text-sm">
+                      {AVAILABLE_PERSONAS.map((model) => (
+                        <Listbox.Option
+                          key={model.value}
+                          className={({ active }) =>
+                            classNames(
+                              "relative cursor-pointer select-none py-2 pl-3 pr-9",
+                              { "bg-zinc-600": active }
+                            )
+                          }
+                          value={model}
+                        >
+                          {({ selected }) => (
+                            <>
                               <span
                                 className={classNames(
-                                  "absolute inset-y-0 right-0 flex items-center pr-2"
+                                  selected ? "font-semibold" : "font-normal",
+                                  "block truncate"
                                 )}
                               >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
+                                {model.name}
                               </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </div>
-          )}
-        </Listbox>
-        <Listbox value={selectedPersona} onChange={setSelectedPersona}>
-          {({ open }) => (
-            <div className="flex items-center space-x-2">
-              <Listbox.Label className="block text-sm leading-6">
-                Persona:
-              </Listbox.Label>
-              <div className="relative w-48">
-                <Listbox.Button className="relative w-full cursor-pointer rounded-md py-1.5 pl-3 pr-10 text-left shadow-sm sm:text-sm sm:leading-6 border border-white/30 hover:border-white/40">
-                  <span className="block truncate">{selectedPersona.name}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ChevronUpDownIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Listbox.Button>
 
-                <Transition
-                  show={open}
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-zinc-700 border border-white/30 py-1 shadow-lg sm:text-sm">
-                    {AVAILABLE_PERSONAS.map((model) => (
-                      <Listbox.Option
-                        key={model.value}
-                        className={({ active }) =>
-                          classNames(
-                            "relative cursor-pointer select-none py-2 pl-3 pr-9",
-                            { "bg-zinc-600": active }
-                          )
-                        }
-                        value={model}
-                      >
-                        {({ selected }) => (
-                          <>
-                            <span
-                              className={classNames(
-                                selected ? "font-semibold" : "font-normal",
-                                "block truncate"
-                              )}
-                            >
-                              {model.name}
-                            </span>
-
-                            {selected ? (
-                              <span
-                                className={classNames(
-                                  "absolute inset-y-0 right-0 flex items-center pr-2"
-                                )}
-                              >
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    "absolute inset-y-0 right-0 flex items-center pr-2"
+                                  )}
+                                >
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
               </div>
-            </div>
-          )}
-        </Listbox>
+            )}
+          </Listbox>
+        </div>
       </div>
       <div className="mt-24 mb-4">What would you like to understand?</div>
       <div className="flex space-x-2 items-center mb-4">
